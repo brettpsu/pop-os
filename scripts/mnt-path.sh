b8a4202aecf83f115121302b126fc75d1970db2e
brettpsu <51752453+brettpsu@users.noreply.github.com>
2022-07-08 16:23:52 -0400

    Update mnt-path.sh
    fixed file placement

--- scripts/mnt-path.sh
+++ scripts/mnt-path.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 echo 'userid:' $1
-echo "//192.168.1.5/$1  /home/$1/pluto  cifs  credentials=/home/$1/.smbcredentials,uid=1000,gid=1000,file_mode=0755,dir_mode=0755  0  0" >> ./test-fstab
+echo "//192.168.1.5/$1  /home/$1/pluto  cifs  credentials=/home/$1/.smbcredentials,uid=1000,gid=1000,file_mode=0755,dir_mode=0755  0  0" >> /etc/fstab
